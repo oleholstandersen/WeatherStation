d2dd1c16e8309371d95a4788da9c5b2a52fd16c0
Ole Holst Andersen <ole.holst.andersen@gmail.com>
2020-05-01 13:28:50 +0200

    Switched TineBME280 library from technoblogy/tiny-bme280 to fabyte/Tiny_BME280_Arduino_Library

--- TPHSensorUnit/tph_sensor_unit/tph_sensor_unit.ino
+++ TPHSensorUnit/tph_sensor_unit/tph_sensor_unit.ino
@@ -1,9 +1,10 @@
+#define TINY_BME280_I2C
 
 #include <RCSwitch.h>
 #include <TinyBME280.h>
 #include <ArduinoUniqueID.h>
 
-#define DELAY_MS 30000      //30 secs
+#define DELAY_MS 10000      //10 secs
 
 #ifndef DELAY_MS
   #define DELAY_MS 300000   //5 mins
@@ -13,6 +14,7 @@
 #define LED_PIN 1
 
 RCSwitch rfSwitch = RCSwitch();
+tiny::BME280 bmp280;
 char idBuf[UniqueIDsize+1];
 uint8_t hash = 0;
 uint8_t messageNo = 0;
@@ -20,13 +22,13 @@ uint8_t messageNo = 0;
 void setup() {                                                                      
   rfSwitch.enableTransmit(RF_PIN);
   pinMode(LED_PIN, OUTPUT);
-  BME280setup();
+  bmp280.beginI2C(0x76);
   for (size_t i = 0; i < UniqueIDsize; i++)
   {
     digitalWrite(LED_PIN, HIGH);
     hash = (hash*31) ^ UniqueID[i];
     idBuf[i] = (UniqueID[i] <= 9 ? '0' + UniqueID[i] : 'A' - 10 + UniqueID[i]);
-    rfSwitch.send(UniqueID[i], 4);
+    //rfSwitch.send(UniqueID[i], 4);
     digitalWrite(LED_PIN, LOW);
     delay(10);
   }
@@ -35,18 +37,34 @@ void setup() {
 
 void loop() {
   digitalWrite(LED_PIN, HIGH);
+  /*
   char data[134];
   sprintf(
     data,
-    "{\"temp\": %d,\"pres\": %u,\"hum\": %u,\"id\": \"%s\",\"batt\": %d}",
+    "{\"temp\":%d,\"pres\":%u,\"hum\":%u,\"id\":\"%s\",\"batt\":%d,\"ids\":%d}",
     BME280temperature(), 
     BME280pressure(),
     BME280humidity(),
     idBuf,
-    min(22528/analogRead(12),100));
+    min(22528/analogRead(12),100),
+    UniqueIDsize);
   sendString(data);
+  */
+  sendInt32(bmp280.readFixedTempC(), 1);
+  sendInt32(bmp280.readFixedPressure(), 2);
+  sendInt32(bmp280.readFixedHumidity(), 3);
+  sendInt32(min(22528/analogRead(12),100),4);
+  sendInt32(UniqueIDsize,5);
   digitalWrite(LED_PIN, LOW);
   delay(DELAY_MS);//Wait for the defined delay
+}
+
+void sendInt32(int32_t val, int8_t no) {
+  val = val | (((int32_t)no) << 24);
+  if (val == 0)  {
+    val = 0xFFFFFFFF;
+  }
+  rfSwitch.send(val, 32);
 }
 
 void sendString(char *str) {
